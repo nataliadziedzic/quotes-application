4ddbaa025096c3c03fa234adb716935cb5a94568
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-08-04 15:56:10 +0200

    Updates

---
+++ precache-manifest.b02569bf01fe29bbde83d113616261c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf1b54eb9e009bc26a69a7031c84aca",
+    "url": "/quotes-application/index.html"
+  },
+  {
+    "revision": "9c563434f632c2f458de",
+    "url": "/quotes-application/static/css/main.02a96b8e.chunk.css"
+  },
+  {
+    "revision": "8ddf36f11bd8bea8173e",
+    "url": "/quotes-application/static/js/2.f9f1dccc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/quotes-application/static/js/2.f9f1dccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c563434f632c2f458de",
+    "url": "/quotes-application/static/js/main.69ded2b8.chunk.js"
+  },
+  {
+    "revision": "917dfb2e37b9871bbd40",
+    "url": "/quotes-application/static/js/runtime-main.fd0b10d4.js"
+  },
+  {
+    "revision": "adf3cf351a7fd2fc6d48159ecba5c7b0",
+    "url": "/quotes-application/static/media/achieve-sunset.adf3cf35.jpg"
+  }
+]);
