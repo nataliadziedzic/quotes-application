9091d654e3452372b513bab85c60b451d32f5a5d
Natalia Dziedzic <nataliadziedzic47@o2.pl>
2020-09-06 16:47:48 +0200

    Add form validation and update styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,45 @@
 import React from 'react';
 function Contact() {
-    function clear(e) {
-        e.preventDefault()
-        document.querySelector('form').reset()
+    let emailIsCorrect = false
+    // Check if each field is completed
+    const handleSend = (e) => {
+        let isAllCompleted = [];
+        const userData = document.querySelectorAll(".contact__input");
+        userData.forEach(input => {
+            if (input.value !== "") {
+                isAllCompleted.push("ok")
+                return isAllCompleted
+            }
+        })
+        if (isAllCompleted.length === 3 && emailIsCorrect) {
+            const div = document.querySelector(".contact__messageSucceed")
+            const opacity = document.querySelector(".opacity")
+            div.style.display = "block"
+            opacity.style.backgroundColor = 'rgba(0, 0, 0, .5)'
+            setTimeout(() => {
+                document.querySelector('form').reset()
+                opacity.style.backgroundColor = 'rgba(0, 0, 0, .2)'
+                div.style.display = "none"
+            }, 1500)
+        }
 
     }
-
+    // Check if email is typed correctly - must include '@' and characters before and after it.
+    const handleCheck = (e) => {
+        emailIsCorrect = false
+        console.log(e.target.value.length)
+        if (e.target.value.indexOf("@") !== -1 && e.target.value.indexOf("@") !== 0 && e.target.value.indexOf("@") < (e.target.value.length - 1)) {
+            emailIsCorrect = true
+        }
+    }
     return (
         <div className="contact">
             <form className="contact__form">
-                <input className="contact__input" type="text" placeholder="Enter your name" />
-                <input className="contact__input" type="text" placeholder="e-mail" />
-                <textarea className="contact__message" placeholder="message..."></textarea>
-                <button className="contact__btn" onClick={clear}>Send</button>
+                <input className="contact__input" type="text" placeholder="Enter your name" required />
+                <input className="contact__input" type="email" onChange={handleCheck} placeholder="e-mail" required />
+                <textarea className="contact__input contact__message" placeholder="message..." required></textarea>
+                <button type="submit" className="contact__btn" onClick={handleSend}>Send</button>
+                <div className="contact__messageSucceed">Thanks for your message!</div>
             </form>
             <div className="contact__informations">
                 <h2 className="contact__info"><i className="far fa-envelope"> </i> mymotivation@.com</h2>
