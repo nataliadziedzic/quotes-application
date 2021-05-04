66080732e5993afbb7e50030861e027923ad482d
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-05-04 11:44:20 +0200

    Extract intro text to a variable

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,33 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { introductionText } from '../../variables';
 
 const HomePage = () => {
   const permission = useSelector((state) => state.permissionReducer);
-  const text = [];
-  const text1 =
-    'Does lack of motivation prevent you from doing what you want in life?';
-  const text2 =
-    'We know that feeling and it can happen to everyone. The key is to look for solutions and this is when your new app comes to the rescue!';
-  const text3 =
-    'We will make sure to maintain the right level of motivation and physical activity in your life.';
   const [page, setPage] = useState(1);
+
   const handleNext = () => {
     setPage(page + 1);
   };
-  if (page === 1) {
-    text.push(text1);
-  } else if (page === 2) {
-    text.splice(0, 1);
-    text.push(text2);
-  } else {
-    text.splice(0, 1);
-    text.push(text3);
-  }
+
   return (
     <div className="home">
       <h1 className="home__title">Fight for yourself!</h1>
-      <p className="home__intro"> {text} </p>
+      <p className="home__intro"> {introductionText[page - 1]} </p>
       {page !== 3 ? (
         <button type="button" className="home__next" onClick={handleNext}>
           Next
---
+++ src/variables.js
@@ -0,0 +1,5 @@
+export const introductionText = [
+  'Does lack of motivation prevent you from doing what you want in life?',
+  'We know that feeling and it can happen to everyone. The key is to look for solutions and this is when your new app comes to the rescue!',
+  'We will make sure to maintain the right level of motivation and physical activity in your life.',
+];
