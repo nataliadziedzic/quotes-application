56c6865521a123906ca3b61f66e51869d510a969
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-05-05 21:40:04 +0200

    Add new error message

--- src/variables.js
+++ src/variables.js
@@ -3,3 +3,10 @@ export const introductionText = [
   'We know that feeling and it can happen to everyone. The key is to look for solutions and this is when your new app comes to the rescue!',
   'We will make sure to maintain the right level of motivation and physical activity in your life.',
 ];
+export const errors = {
+  loginError: 'Your username or password is incorrect',
+  emptyFieldError: 'Fields cannot be empty',
+  usernameError: 'Username should be at least 2 signs long',
+  emailError: 'E-mail is not correct',
+  passwordError: 'Password cannot be shorter than 6 signs',
+};
